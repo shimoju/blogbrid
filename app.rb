8d73a138cbf9a50f0616ce34d001cede800dd4cc
Hiroshi Shimoju <hiroshi.shimoju@gmail.com>
2015-02-23 00:25:37 +0900

    Use Sinatra::Reloader

--- app.rb
+++ app.rb
@@ -3,6 +3,11 @@ require 'bundler/setup'
 Bundler.require(:default, ENV['RACK_ENV'])
 
 class Blogbrid < Sinatra::Base
+  configure :development do
+    require 'sinatra/reloader'
+    register Sinatra::Reloader
+  end
+
   get '/' do
     'Hello Blogbrid!'
   end
